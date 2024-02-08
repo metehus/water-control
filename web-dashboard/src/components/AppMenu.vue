521d672cea37a52285ead054d156c6a4baedcf15
Matheus Dias <methidezeeni@gmail.com>
2024-02-08 00:52:29 -0300

    feat: vue dashboard

---
+++ web-dashboard/src/components/AppMenu.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+import Menubar from "primevue/menubar";
+import { ref } from "vue";
+
+const items = ref([
+  {
+    label: 'Início',
+    icon: 'home'
+  },
+  {
+    label: 'Configurações',
+    icon: 'settings'
+  }
+]);
+
+</script>
+
+<template>
+  <nav>
+    <Menubar :model="items">
+      <template #itemicon="{ item }">
+        <vue-feather class="menu-icon" :type="item.icon"></vue-feather>
+      </template>
+    </Menubar>
+  </nav>
+</template>
+
+<style scoped>
+.menu-icon {
+  margin-right: 8px;
+}
+nav {
+  max-width: 1280px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
